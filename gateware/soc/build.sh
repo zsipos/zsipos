71475ff0b294f9098eb25e12111de265f8b975bc
Stefan Adams <stefan.adams@vipcomag.de>
2019-10-21 14:56:18 +0200

    generate devicetree

--- gateware/soc/build.sh
+++ gateware/soc/build.sh
@@ -3,6 +3,7 @@ outdir=build_$BOARD
 python3 $BOARD.py \
 	--cpu-type=rocket${BITS} --cpu-variant=linux \
 	--output-dir $outdir \
+	--dts-file $outdir/software/include/generated/devicetree.dts \
  	--csr-json $outdir/$BOARD.json \
 	$nobuild
 
