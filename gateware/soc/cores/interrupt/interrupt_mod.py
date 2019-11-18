779cd451244d1f6ba994a2d54114f881b164886f
Stefan Adams <stefan.adams@vipcomag.de>
2019-11-18 17:53:15 +0100

    rename intterupt module

--- gateware/soc/cores/interrupt/interrupt_mod.py
+++ gateware/soc/cores/interrupt/interrupt_mod.py
@@ -2,15 +2,17 @@
 from migen import *
 
 
-class IRQ():
+class _IRQ():
     def __init__(self):
         self.irq = Signal(1)
 
+class Interrupt(Module):
+    def __init__(self):
+        self.ev = _IRQ()
 
-class EXTINT(Module):
-
+class ExtInterrupt(Interrupt):
     def __init__(self, platform, name, number=None):
-        self.ev = IRQ()
+        Interrupt.__init__(self)
         pin = platform.request(name, number)
         self.comb += self.ev.irq.eq(pin)
 
