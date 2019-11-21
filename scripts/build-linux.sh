701c3162e602998204694b5f7e45ad7a54792cc8
Stefan Adams <stefan.adams@vipcomag.de>
2019-11-21 09:54:37 +0100

    update 32bit and 64bit config

--- scripts/build-linux.sh
+++ scripts/build-linux.sh
@@ -5,7 +5,8 @@ cd "$ZTOP/kernel"
 
 SRCDIR="`pwd`/linux"
 WRKDIR="`pwd`/build_$BITS/linux"
-CONFIG="zsipos_$BITS"
+CONFIGDIR="$SRCDIR/arch/riscv/configs"
+DEFCONFIG="zsipos_${BITS}_defconfig"
 
 INITRAMFS="$ZTOP/kernel/build_$BITS/initramfs.cpio"
 
@@ -26,7 +27,6 @@ linuxmake()
 if [ ! -f "$WRKDIR/.config" ]
 then
 	echo "using default config"
-	DEFCONFIG=${CONFIG}_defconfig
 	mkdir -p "$WRKDIR"
 	linuxmake $DEFCONFIG
 fi
@@ -35,7 +35,14 @@ if [ "$1" == "config" ]
 then
 	linuxmake menuconfig
 	linuxmake savedefconfig
-	cp "$WRKDIR/defconfig" "$SRCDIR/arch/riscv/configs/${CONFIG}_defconfig"
+	cp "$WRKDIR/defconfig" "$CONFIGDIR/$DEFCONFIG"
+	if [ "$BITS" == 64 ]
+	then
+		echo "CONFIG_ARCH_RV32I=y"   >"$CONFIGDIR/zsipos_32_defconfig"
+		cat "$CONFIGDIR/$DEFCONFIG" >>"$CONFIGDIR/zsipos_32_defconfig"
+	else
+		grep -v "CONFIG_ARCH_RV32I" <"$CONFIGDIR/$DEFCONFIG" >"$CONFIGDIR/zsipos_64_defconfig"
+	fi
 else
 	linuxmake -j8 vmlinux
 fi
