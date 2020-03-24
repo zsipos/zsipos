9ef27f79e382092ccee1d81760b258d1f3a82a1b
Stefan Adams <stefan.adams@vipcomag.de>
2020-03-24 09:08:05 +0100

    more dependencies

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,1 +1,1 @@
-sudo apt-get install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev python3-venv python3-dev libyaml-dev libssl-dev pkg-config libglib2.0-dev
+sudo apt-get install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev python3-venv python3-dev libyaml-dev libssl-dev pkg-config libglib2.0-dev libpixman-1-dev
