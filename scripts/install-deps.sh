270a412bcdb947c4ec49a8a529cf8bb19d35d3f5
Stefan Adams <stefan.adams@vipcomag.de>
2020-03-24 11:00:17 +0100

    split long line

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,4 +1,11 @@
-sudo apt-get install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev python3-venv python3-dev libyaml-dev libssl-dev pkg-config libglib2.0-dev libpixman-1-dev ninja-build device-tree-compiler libxml2-utils ccache ncurses-dev doxygen u-boot-tools protobuf-compiler python-protobuf
+sudo apt-get install \
+	autoconf automake autotools-dev curl python3 libmpc-dev \
+	libmpfr-dev libgmp-dev gawk build-essential bison flex \
+	texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev \
+	python3-venv python3-dev libyaml-dev libssl-dev pkg-config \
+	libglib2.0-dev libpixman-1-dev ninja-build device-tree-compiler \
+	libxml2-utils ccache ncurses-dev doxygen u-boot-tools \
+	protobuf-compiler python-protobuf
 
 
 
