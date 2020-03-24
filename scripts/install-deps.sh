a0f5aba61e1f775d8308f97e071e0d4a9db31900
Stefan Adams <stefan.adams@vipcomag.de>
2020-03-24 09:26:45 +0100

    fix: ninja package is ninja-build

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -1,2 +1,3 @@
-sudo apt-get install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev python3-venv python3-dev libyaml-dev libssl-dev pkg-config libglib2.0-dev libpixman-1-dev ninja
+sudo apt-get install autoconf automake autotools-dev curl python3 libmpc-dev libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo gperf libtool patchutils bc zlib1g-dev libexpat-dev python3-venv python3-dev libyaml-dev libssl-dev pkg-config libglib2.0-dev libpixman-1-dev ninja-build
+
 
