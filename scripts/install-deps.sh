039e318f2d73e3d9797a8f46f66158b81044193b
Stefan Adams <stefan.adams@vipcomag.de>
2020-05-22 11:22:58 +0200

    add haskell-stack

--- scripts/install-deps.sh
+++ scripts/install-deps.sh
@@ -6,8 +6,8 @@ sudo apt-get install \
 	libglib2.0-dev libpixman-1-dev ninja-build device-tree-compiler \
 	libxml2-utils ccache ncurses-dev doxygen u-boot-tools \
 	protobuf-compiler python-protobuf libx11-dev libxinerama-dev \
-	libfreetype6-dev libxft-dev uuid-dev
+	libfreetype6-dev libxft-dev uuid-dev haskell-stack
 
 
-
-
+#sudo apt-get install clang libclang-dev libcunit1-dev libsqlite3-dev
+#sudo apt-get install gdb qemu-kvm
