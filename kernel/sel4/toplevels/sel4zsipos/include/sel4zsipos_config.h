719eded5411139cecb11638e569c7ba0e388ef88
Stefan Adams <stefan.adams@vipcomag.de>
2020-09-08 17:41:22 +0200

    add spdx headers

--- kernel/sel4/toplevels/sel4zsipos/include/sel4zsipos_config.h
+++ kernel/sel4/toplevels/sel4zsipos/include/sel4zsipos_config.h
@@ -1,3 +1,6 @@
+// SPDX-FileCopyrightText: 2020 Stefan Adams <stefan.adams@vipcomag.de>
+// SPDX-License-Identifier: GPL-2.0-or-later
+
 #ifndef _h_sel4zsipos_config_h
 #define _h_sel4zsipos_config_h
 
