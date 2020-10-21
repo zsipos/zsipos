31b3d60f8a3b526f699c46a1e14919bab0536075
Stefan Adams <stefan.adams@vipcomag.de>
2020-10-21 10:14:49 +0200

    fix clean option

--- scripts/build-zsipos.sh
+++ scripts/build-zsipos.sh
@@ -4,18 +4,15 @@
 set -e
 echo "building zsipos application ..."
 
-gitstamps.sh
-
 build-sel4iptool.sh $1
 
 if [ "$1"x == "clean"x ]
 then
 	m=clean
-	exit
 else
+	gitstamps.sh
 	m=
 fi
-
 
 (cd "$ZTOP/software/zsipos/gui"; make $m)
 (cd "$ZTOP/software/zsipos/rtp"; make $m)
